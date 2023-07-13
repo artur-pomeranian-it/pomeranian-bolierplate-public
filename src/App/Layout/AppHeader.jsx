eff570e1a410ab56c211341c45037e19b2bcac73
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-13 18:53:20 +0200

    dodaj logo

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/header.css';
+import { Logo } from '../Components/Logo/Logo';
 
 export function AppHeader() {
   return (
     <header>
+      <Link to="/">
+        <Logo />
+      </Link>
       <div>
         <div className="header-title">Tytuł nagłówka</div>
       </div>
