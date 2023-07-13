36341f00ee48c882fd2545ce01ccfcb3d2d084e3
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-13 19:31:07 +0200

    dodaj cv template

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { NotFound } from './App/Components/NotFound/NotFound';
 import { Blocks } from './App/Blocks';
 import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
+import { CV } from './App/CV/CV';
 import { Exercises } from './App/Exercises';
 
 export function App() {
@@ -15,6 +16,7 @@ export function App() {
         <Route path="" element={<Layout withSidebar />}>
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard/*" element={<Dashboard />} />
+          <Route path="cv/*" element={<CV />} />
           <Route path="exercises/*" element={<Exercises />} />
           <Route path="blocks/*" element={<Blocks />} />
           <Route path="*" element={<NotFound />} />
