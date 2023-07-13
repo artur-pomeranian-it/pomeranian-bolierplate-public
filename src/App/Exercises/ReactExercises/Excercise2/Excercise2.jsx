0fd5793e18ef8619d3f9d2ef00a89898fc022ce8
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-13 23:56:55 +0200

    excercise 2 completed

--- src/App/Exercises/ReactExercises/Excercise2/Excercise2.jsx
+++ src/App/Exercises/ReactExercises/Excercise2/Excercise2.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export function Excercise2() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  function handleClick(event) {
+    setCount(count + 1);
+  }
+
   return (
     <div className="excercise-2">
       <h1 className="h1">Cześć!</h1>
-      <button className="button">kliknięto we mnie 0 razy</button>
+      <button className="button" onClick={handleClick}>
+        kliknięto we mnie {count} razy
+      </button>
     </div>
   );
 }
