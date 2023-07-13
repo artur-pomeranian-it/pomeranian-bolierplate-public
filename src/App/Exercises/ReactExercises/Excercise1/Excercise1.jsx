4dd0b92aa163463d68f34577612fa32c87832d52
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-13 23:49:15 +0200

    excercise-1 completed

--- src/App/Exercises/ReactExercises/Excercise1/Excercise1.jsx
+++ src/App/Exercises/ReactExercises/Excercise1/Excercise1.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export function Excercise1() {
+  const defaultText = 'nie kliknięto we mnie';
+  const [buttonText, setButtonText] = useState(defaultText);
+
+  function handleClicked(event) {
+    console.log('kliknieto', event);
+    setButtonText('kliknięto we mnie');
+  }
+
   return (
     <div className="excercise-1">
       <h1 className="h1">Cześć!</h1>
-      <button className="button">nie kliknięto we mnie</button>
+      <button className="button" onClick={handleClicked}>
+        {buttonText}
+      </button>
     </div>
   );
 }
