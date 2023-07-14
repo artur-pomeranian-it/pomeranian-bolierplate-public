da0ae1e4d17ce108df092dc62ef3588817858ceb
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-14 13:58:02 +0200

    block 9 excercise 4

--- src/App/Exercises/ReactExercises - moreOrLessAdvanced/Block9Excercise4.jsx
+++ src/App/Exercises/ReactExercises - moreOrLessAdvanced/Block9Excercise4.jsx
@@ -3,41 +3,59 @@ import React, { useState } from 'react';
 import './style.css';
 
 export function Block9Excercise4() {
-  const [inputText, setInputText] = useState('');
-  const [placeholderText, setPlaceholderText] = useState('');
+  const [inputTextA, setInputTextA] = useState('A');
+  const [inputTextB, setInputTextB] = useState('B');
+  const [answer, setAnswer] = useState('XX');
 
-  function handleChange(event) {
-    setInputText(event.target.value);
+  function handleChangeA(event) {
+    const text = event.target.value;
+    setInputTextA(text);
+    setABAnswer(text, inputTextB);
   }
 
-  function handleClick(event) {
-    setPlaceholderText(placeholderText + ' ' + inputText);
-    setInputText('');
+  function handleChangeB(event) {
+    const text = event.target.value;
+    setInputTextB(text);
+    setABAnswer(inputTextA, text);
+  }
+
+  function setABAnswer(textA, textB) {
+    const numberA = parseInt(textA, 10);
+    const numberB = parseInt(textB, 10);
+    const response = numberA > numberB ? numberA : numberB;
+    if (isNaN(response)) {
+      setAnswer('Niepoprawne dane');
+    } else {
+      setAnswer(response + ' jest większe');
+    }
   }
 
   return (
-    <div className="block-9-excercise-1">
+    <div className="block-9-excercise-3">
       <div className="wrapper">
-        <h1 className="h1">Cześć!</h1>
-        <div className="form" action="">
-          <label className="label" htmlFor="text1_id">
-            Wpisz tekst
-          </label>
-          <input
-            className="input"
-            type="text"
-            name="text1"
-            id="text1_id"
-            placeholder="tekst do wpisania"
-            onChange={handleChange}
-            value={inputText}
-          />
-        </div>
-        <button type="submit" className="button" onClick={handleClick}>
-          kliknij
-        </button>
+        <h1 className="h1">
+          Czy {inputTextA} jest większe od {inputTextB}?
+        </h1>
+        <input
+          className="input"
+          type="text"
+          name="inputTextA"
+          id="inputTextA"
+          placeholder="tekst do wpisania"
+          onChange={handleChangeA}
+          value={inputTextA}
+        />
+        <input
+          className="input"
+          type="text"
+          name="inputTextB"
+          id="inputTextB"
+          placeholder="tekst do wpisania"
+          onChange={handleChangeB}
+          value={inputTextB}
+        />
+        <div className="placeholder">Odpowiedź: {answer}</div>
       </div>
-      <div className="placeholder">{placeholderText}</div>
     </div>
   );
 }
