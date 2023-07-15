cc3adc6d017e7c0f0e33384e524e7a825b9b53ad
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-15 09:08:48 +0200

    block 12 colors

---
+++ src/App/Exercises/HtmlCss/Colors/router-data.jsx
@@ -0,0 +1,10 @@
+import { Colors } from './Colors';
+
+export const blockRouterMetaData = {
+  path: 'some-path-that-you-wants',
+  date: '15-07-2023',
+  linkLabel: 'Kolory CSS',
+  blockNo: 12,
+  element: <Colors />,
+  tags: ['css', 'colors'],
+};
