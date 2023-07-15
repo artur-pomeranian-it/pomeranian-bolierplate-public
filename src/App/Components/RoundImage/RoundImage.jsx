17f90b29fcabc4e98ab91578da300e5836370fbb
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-15 12:41:49 +0200

    block 12 round image

--- src/App/Components/RoundImage/RoundImage.jsx
+++ src/App/Components/RoundImage/RoundImage.jsx
@@ -1,10 +1,11 @@
 import './styles.css';
 
-export const RoundImage = () => {
+export const RoundImage = ({ size }) => {
   return (
     <div
       className="round-image-wolf"
-      style={{ borderRadius: '100%', height: '150px', width: '150px' }}
+      // style={{ borderRadius: '100%', height: '150px', width: '150px' }}
+      style={{ borderRadius: '100%', height: `${size}px`, width: `${size}px` }}
       alt="wolf"
     />
   );
