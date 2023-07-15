34b972b98ce12cd438d4b34e594b42df68441aef
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-15 09:34:47 +0200

    images

---
+++ src/App/Exercises/HtmlCss/ImageFiles/router-data.jsx
@@ -0,0 +1,10 @@
+import { ImageFiles } from './ImageFiles';
+
+export const blockRouterMetaData = {
+  path: 'image-files',
+  date: '15-07-2023',
+  linkLabel: 'Pliki graficzne',
+  blockNo: 12,
+  element: <ImageFiles />,
+  tags: ['html', 'image'],
+};
