1994cf1e25c333d4f5ecf476dfa2fac60d9ddc26
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-16 08:21:02 +0200

    block 14 tables

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -8,6 +8,8 @@ import { blockRouterMetaData as Backgrounds } from './Backgrounds/router-data';
 import { blockRouterMetaData as CssFilter } from './CssFilter/router-data';
 import { blockRouterMetaData as MediaFiles } from './MediaFiles/router-data';
 import { blockRouterMetaData as BoxModel } from './BoxModel/router-data';
+import { blockRouterMetaData as HTMLTables } from './HTMLTables/router-data';
+import { blockRouterMetaData as TablesTennis } from './TablesTennis/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
@@ -18,5 +20,7 @@ export const blockRouterMetaData = [
   CssFilter,
   MediaFiles,
   BoxModel,
+  HTMLTables,
+  TablesTennis,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
