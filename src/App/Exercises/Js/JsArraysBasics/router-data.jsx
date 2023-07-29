61ecf54fe67579e03017c06f4fc063b27e758e9b
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-07-29 12:58:42 +0200

    the end

---
+++ src/App/Exercises/Js/JsArraysBasics/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsArraysBasics } from './JsArraysBasics';
+
+export const blockRouterMetaData = {
+  path: 'js-arrays-basics',
+  date: '12-12-2023',
+  linkLabel: 'Tablice w js',
+  blockNo: 20,
+  element: <JsArraysBasics />,
+  tags: ['arrays', 'javascript'],
+};
---
+++ src/App/Exercises/Js/JsDateTime/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsDateTime } from './JsDateTime';
+
+export const blockRouterMetaData = {
+  path: 'date-and-time',
+  date: '12-12-2023',
+  linkLabel: 'Blok 20 - Data i czas',
+  blockNo: 20,
+  element: <JsDateTime />,
+  tags: ['timestamp', 'date', 'javascript'],
+};
