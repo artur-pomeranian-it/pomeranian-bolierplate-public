523108de0aba6e2c5c8463808187bc663ac4f8da
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-08-30 20:40:30 +0200

    testy msw

--- src/App/Exercises/Js/Testing/ToDoList.test.js
+++ src/App/Exercises/Js/Testing/ToDoList.test.js
@@ -1,5 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/lib/node';
 import { getRandomInt, ToDoList } from './TodoList';
+import { act } from 'react-dom/test-utils';
+
+const testData = [
+  {
+    id: 105,
+    title: 'Research Topic',
+    note: 'Research Topic',
+    author: 'Anonymous',
+    isDone: false,
+    createdAt: '2023-08-25T10:36:22.070Z',
+  },
+  {
+    id: 106,
+    title: 'Grocery Shopping',
+    note: 'Grocery Shopping',
+    author: 'Anonymous',
+    isDone: false,
+    createdAt: '2023-08-25T10:36:34.700Z',
+  },
+];
+
+const baseURL = 'http://localhost:3333';
+const basePath = `${baseURL}/api/todo`;
+const DELAY = 100;
+const server = setupServer(
+  rest.get(basePath, (_req, res, ctx) => {
+    return res(ctx.delay(DELAY), ctx.json(testData));
+  }),
+  rest.post(basePath, (req, res, ctx) => {
+    return res(ctx.delay(DELAY), ctx.json({ title: 'TEST' }));
+  }),
+  rest.delete(`${basePath}/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+    return res(ctx.delay(DELAY), ctx.json({ id }));
+  })
+);
 
 describe('getRandomIng', () => {
   // 5  => random = 0.9999 => 4
@@ -22,9 +66,66 @@ describe('getRandomIng', () => {
 });
 
 describe('ToDoList', () => {
-  it('has header = todo list', () => {
+  beforeAll(() => {
+    // jest.spyOn(window, 'fetch');
+    server.listen();
+  });
+  afterAll(() => {
+    server.close();
+    // jest.restoreAllMocks();
+  });
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  it('has header = todo list', async () => {
+    // fetch.mockImplementation(() =>
+    //   Promise.resolve({ ok: true, json: () => Promise.resolve(testData) })
+    // );
     render(<ToDoList />);
-    const heading = screen.getByRole('heading', { name: /todo list/i });
+    // await act(async () => {
+    const heading = await screen.findByRole('heading', {
+      name: /todo list/i,
+    });
     expect(heading).toBeInTheDocument();
+    // });
+  });
+  it('has refresh button', async () => {
+    // fetch.mockImplementation(() =>
+    //   Promise.resolve({ ok: true, json: () => Promise.resolve(testData) })
+    // );
+    render(<ToDoList />);
+    // await act(async () => {
+    const refreshButton = screen.getByRole('button', {
+      name: /refresh/i,
+    });
+    expect(refreshButton).toBeInTheDocument();
+    // });
+  });
+  it('returns list with elements on load', async () => {
+    // fetch.mockImplementation(() =>
+    //   Promise.resolve({ ok: true, json: () => Promise.resolve(testData) })
+    // );
+    render(<ToDoList />);
+    // await act(async () => {
+    const refreshButton = screen.getByRole('button', {
+      name: /refresh/i,
+    });
+    expect(refreshButton).toBeInTheDocument();
+    // });
+    const row = await screen.findByRole('row', {
+      name: /grocery shopping 25\.08\.2023, 12:36 delete/i,
+    });
+
+    const deleteButton = within(row).getByRole('button', {
+      name: /delete/i,
+    });
+
+    // expect(deleteButton).toBeInTheDocument();
+    fireEvent.click(deleteButton);
+    await waitFor(() =>
+      expect(screen.getByText(/usunięto zadanie/i)).toBeInTheDocument()
+    );
+    screen.logTestingPlaygroundURL();
   });
 });
