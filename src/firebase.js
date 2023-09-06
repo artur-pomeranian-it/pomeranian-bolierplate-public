1d75e57cdaf80f4f709a5b63799930cb78ee69b5
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-09-06 16:05:36 +0200

    firebase test

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAcC-ScoS5AmRq3o5-9hA45ei6juIahuPI',
-  authDomain: 'pomeranian-fire.firebaseapp.com',
-  projectId: 'pomeranian-fire',
-  storageBucket: 'pomeranian-fire.appspot.com',
-  messagingSenderId: '692222167618',
-  appId: '1:692222167618:web:19b33a65634505f0457bdb',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './index.css';
 import { App } from './App';
 import { store } from './App/Store';
 // import { worker } from './App/Mocks/setupWorker';
+import app from './firebase';
 
 if (process.env.NODE_ENV === 'development') {
   // worker.start();
 }
-
+console.log('Firebase App Name: ', app.name);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
