594ac5a53f65026addd4d1b3456bfe31aa6da18d
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-09-06 23:44:54 +0200

    cocked todo

--- src/App/Mocks/ToDo/handlers.js
+++ src/App/Mocks/ToDo/handlers.js
@@ -1,74 +1,63 @@
 import { rest } from 'msw';
-import { testData } from './testData';
-
+import {
+  addToDo,
+  deleteToDo,
+  getFromLocalStorage,
+  getToDo,
+  markAsDone,
+  updateToDo,
+} from './localStorage';
 export const baseURL = 'http://localhost:3333';
 export const basePath = `${baseURL}/api/todo`;
 export const DELAY = 10;
-
-const localStorageName = 'todo-api';
-
-const getFromLocalStorage = () => {
-  const storedData = localStorage.getItem('localStorageName');
-  return storedData ? JSON.parse(storedData) : testData;
-};
-
-const saveToLocalStorage = (data) => {
-  localStorage.setItem(localStorageName, JSON.stringify(data));
-};
-
-const addToDoToLocalStorage = (todo) => {
-  const data = getFromLocalStorage();
-  data.push(todo);
-  saveToLocalStorage(data);
-};
-
-const deleteToDoFromLocalStorage = (id) => {
-  const data = getFromLocalStorage();
-  saveToLocalStorage(data.filter((todo) => todo.id !== id));
-};
-
-const markToDoAsDone = (id) => {
-  const data = getFromLocalStorage();
-  const newData = data.map((todo) =>
-    todo.id !== id
-      ? todo
-      : { ...todo, isDone: true, doneDate: new Date().toISOString() }
-  );
-  console.log(
-    'newData',
-    newData,
-    id,
-    data.find((todo) => todo.id === id)
-  );
-  saveToLocalStorage(newData);
-  return newData.find((todo) => todo.id === id);
-};
 
 const mockGet = rest.get(basePath, (_req, res, ctx) => {
   const data = getFromLocalStorage();
   return res(ctx.delay(DELAY), ctx.json(data));
 });
-const mockPost = rest.post(basePath, (req, res, ctx) => {
-  const newToDo = JSON.parse(req.bodyUsed);
-  addToDoToLocalStorage(newToDo);
+
+const mockGetOne = rest.get(`${basePath}/:id`, (req, res, ctx) => {
+  const id = parseInt(req.params?.id);
+  const todo = getToDo(id);
+  return res(ctx.delay(DELAY), ctx.json(todo));
+});
+
+const mockPost = rest.post(basePath, async (req, res, ctx) => {
+  const body = await req.text();
+  const reqToDo = JSON.parse(body);
+  const newToDo = addToDo(reqToDo);
   return res(ctx.delay(DELAY), ctx.json(newToDo));
 });
+
 const mockDelete = rest.delete(`${basePath}/:id`, (req, res, ctx) => {
-  const { id } = req.params;
-  const intId = JSON.parse(id);
-  deleteToDoFromLocalStorage(intId);
+  const id = parseInt(req.params?.id);
+  deleteToDo(id);
   return res(ctx.delay(DELAY), ctx.json({ id }));
 });
 
 const mockMarkAsDone = rest.put(
   `${basePath}/:id/markAsDone`,
   (req, res, ctx) => {
-    const { id } = req.params;
-    const intId = JSON.parse(id);
-    const newToDo = markToDoAsDone(intId);
+    const id = parseInt(req.params?.id);
+    const newToDo = markAsDone(id);
     return res(ctx.delay(DELAY), ctx.json(newToDo));
   }
 );
 
-const handlers = [mockGet, mockPost, mockDelete, mockMarkAsDone];
+const mockUpdate = rest.put(`${basePath}/:id`, async (req, res, ctx) => {
+  const id = parseInt(req.params?.id);
+  const body = await req.text();
+  const newToDo = JSON.parse(body);
+  updateToDo(id, newToDo);
+  return res(ctx.delay(DELAY), ctx.json(newToDo));
+});
+
+const handlers = [
+  mockGet,
+  mockGetOne,
+  mockPost,
+  mockDelete,
+  mockMarkAsDone,
+  mockUpdate,
+];
 export default handlers;
